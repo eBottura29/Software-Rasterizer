cced6a99b3fdbc02353bae84ff5809140e58f8d4
eBottura29 <botturaenrico@gmail.com>
2025-08-20 21:55:30 +0200

    updated to support a cube

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-from image import Image
-from image import write_to_bmp
-from floats import float2, float3
-import maths
-from tris import tri2
+import os
+import random
+
+from classes import CubeModel, RenderTarget, float3
+from parsers import obj_parser
+from renderer import render
 
 
-def create_test_image(img):
-    a = float2(0.2 * img.width, 0.2 * img.height)
-    b = float2(0.7 * img.width, 0.4 * img.height)
-    c = float2(0.4 * img.width, 0.8 * img.height)
+# old code for creating a test image, commented out
+# def create_test_image(img):
+#     a = float2(0.2 * img.width, 0.2 * img.height)
+#     b = float2(0.7 * img.width, 0.4 * img.height)
+#     c = float2(0.4 * img.width, 0.8 * img.height)
 
-    for y in range(img.height):
-        for x in range(img.width):
-            inside = maths.point_in_triangle(tri2(a, b, c), float2(x, y))
+#     for y in range(img.height):
+#         for x in range(img.width):
+#             inside = maths.point_in_triangle(tri2(a, b, c), float2(x, y))
 
-            if inside:
-                img.image[x][y] = float3(x=255, y=255, z=255)
+#             if inside:
+#                 img.image[x][y] = float3(x=255, y=255, z=255)
 
-    return img
-
-
-def render(img):
-    write_to_bmp(img)
+#     return img
 
 
-def main(img):
-    img_final = create_test_image(img)
-    render(img_final)
+def main():
+    obj_path = os.getcwd() + "\\obj\\cube.obj"
+    print(f"Loading OBJ file from: {obj_path}")
+    cube_model_vertices = obj_parser(obj_path)
+
+    triangle_cols = [
+        float3(x=random.randint(0, 255), y=random.randint(0, 255), z=random.randint(0, 255)) for _ in range(len(cube_model_vertices) // 3)
+    ]
+
+    cube_model = CubeModel(cube_model_vertices, triangle_cols)
+    render_target = RenderTarget(w=64, h=64, background_color=float3(0, 0, 0))
+
+    render(cube_model, render_target)
 
 
 if __name__ == "__main__":
-    img = Image(size=float2(x=64, y=64), background_color=float3(x=0, y=0, z=0))
-    main(img)
+    main()
